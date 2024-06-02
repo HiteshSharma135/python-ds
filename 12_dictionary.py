918b8b568e7d7b5259e320b84255d2434de74ad3
HiteshSharma135 <Ayansharma135@gmail.com>
2024-06-02 16:52:06 +0530

    dictionary complete

--- 12_dictionary.py
+++ 12_dictionary.py
@@ -142,3 +142,107 @@ print(student)
 
 ###DICTIONARY OPERATIONS
 
+##Membership
+#in dictionary if we put in operations then it will search only in the keys not in values
+
+s={
+    'name':'hitesh',
+    'college':'hbs',
+    'sem':4,
+    'subject':{
+        'dsa':50,
+        'oops':87,
+    }
+}
+
+print('hitesh' in s)#it will give false as it will search in keys
+print('name' in s)#now it will give true as name is the key
+
+##Loops
+#same as membership ,,  loops also works on the keys not on the values
+student={
+    'name':'hitesh',
+    'college':'hbs',
+    'sem':4,
+    'subject':'english'
+}
+
+for i in student:
+    print(i) # now it will print all the keys
+    #print(i,d[i])#to print key and values both(it is not working in vscode but will work in google collab)
+
+
+###DICTIONARY FUNCTIONS
+
+##len/sorted
+student={
+    'name':'hitesh',
+    'college':'hbs',
+    'sem':4,
+    'subject':{
+        'dsa':50,
+        'oops':87,
+    }
+}
+print(len(student))#it will print the length(number) of keys
+
+print(sorted(student))#it will sort the keys in ascending order and give output in list
+
+print(sorted(student,reverse=True))#to sort in descending order
+
+##items/keys/values
+student={
+    'name':'hitesh',
+    'college':'hbs',
+    'sem':4,
+    'subject':{
+        'dsa':50,
+        'oops':87,
+    }
+}
+
+print(student.items())#it will show all the key value pairs in the form of tuple
+
+print(student.keys())#to print all the keys
+
+print(student.values())#to print all the values
+
+##update
+
+d1={1:2,2:4,3:6}
+d2={2:5,3:8}
+
+print(d1.update(d2))#it will update the values of d1 which is present in d2(will work in google collab)
+
+
+###DICTIONARY COMPREHENSION
+
+##program to print 1st 10 numbers and their squares
+
+{i:i**2 for i in range(1,11)}
+
+##using existing dict
+
+distance={'delhi':1000,'mumbai':2000,'banglore':3000}
+
+{key:value*0.62 for (key,value) in distance.items()}#to convert km to miles
+
+##using zip
+days=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"]
+temp_c=[30.5,31.2,33,34.0,35.6,33.2,36.6]
+
+print({i:j for (i,j) in zip(days,temp_c)})
+
+
+##using if condition 
+products={'laptop':10,'phone':15,'tablet':0}
+#to show the products only whose stock is not zero
+
+print({key:value for(key,value) in products.items() if value>0})
+ 
+
+###NESTED COMPREHENSION
+
+##print tables from 2 to 4
+
+print({i:{j:i*j for j in range(1,11)}for i in range(2,5)})
